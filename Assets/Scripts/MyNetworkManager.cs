42a475d3b544680f2d662515e1e404cac3036db4
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-11-13 14:28:23 +0530

    Basic connection, Client-Server(Host) created.

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log("I am connected.");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+        Debug.Log($"Total {numPlayers} players connected.");
+    }
+}
