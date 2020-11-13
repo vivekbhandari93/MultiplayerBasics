3dee53286b171105c8442bf807c67b750cbfd479
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-11-13 16:18:03 +0530

    Information updated on client side also.

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string displayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color displayColor = Color.black;
+
+    [SerializeField] private TextMeshProUGUI displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
     
+
+    [SyncVar(hook =nameof(HandleDisplayNameUpdated))] 
+    [SerializeField] private string displayName = "Missing Name";
+    
+    [SyncVar(hook =nameof(HandleDisplayColorUpdated))] 
+    [SerializeField] private Color displayColor = Color.black;
 
     [Server]
     public void SetDisplayName(string newDisplayName)
@@ -19,5 +27,15 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void SetDisplayColor(Color newDisplayColor)
     {
         displayColor = newDisplayColor;
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
+    }
+    
+    private void HandleDisplayColorUpdated(Color oldColor , Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
     }
 }
